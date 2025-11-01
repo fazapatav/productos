784c802a7d6d33e0bfe47ce9d109ae88f46c2552
fabian.zapata@sofka.com.co <fabian.zapata@sofka.com.co>
2025-10-31 22:03:01 -0500

    se crea proyecto base

---
+++ productos/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>🛒 CRUD de Productos</h1>
+
+      {/* FORMULARIO */}
+      <div className="formulario"></div>
+
+      {/* LISTA DE PRODUCTOS */}
+      <div className="lista"></div>
+    </div>
+  );
+}
+
+export default App;
